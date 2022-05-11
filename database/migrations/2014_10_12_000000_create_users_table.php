6e7339d65809cc1fb8202e7212265bb96c8017c9
abdelrahmandev <abdelrahman@ibtikarat.com.sa>
2022-05-11 16:45:20 +0200

    edit users table with social media login columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('is_admin', ['0','1'])->default(1);
+            $table->string('provider_name')->nullable();
+            $table->string('provider_id')->unique()->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
