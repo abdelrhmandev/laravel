fce1cc9bce88680568357fde79cd28f17edcfa09
Abdelrahman Magdy EL Monshed <amgo123@gmail.com>
2022-05-12 21:54:33 +0200

    master

--- database/migrations/2014_10_12_000000_create_subscriptions_table.php
+++ database/migrations/2014_10_12_000000_create_subscriptions_table.php
@@ -15,7 +15,6 @@ class CreateSubsribtionsTable extends Migration
     {
         Schema::create('subscriptions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamps();
         });
