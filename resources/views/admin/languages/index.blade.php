2a5e14f688ca167010c07f5b5bffd7d0fe0534d4
Abdelrahman Magdy EL Monshed <amgo123@gmail.com>
2022-05-12 16:59:21 +0200

    commit

--- resources/views/admin/languages/index.blade.php
+++ resources/views/admin/languages/index.blade.php
@@ -1,1 +1,20 @@
-dsdas
+<table cellpading="2">
+
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Title</th>
+      </tr>
+    </thead>
+    <tbody>
+        @forelse ($languages as $language)        
+      <tr>
+        <td>{{ $language->id }}</td>
+        <td>{{ $language->title }}</td>
+      </tr>
+      @empty
+      {{  trans('language.no_languages_added') }}
+      @endforelse
+
+    </tbody>
+  </table>
